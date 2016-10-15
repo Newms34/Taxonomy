b6645f44e2c7807eb3431c9b99a72dd709272c2d
newms34 <newms3450@gmail.com>
2016-10-14 20:07:01 -0400

    allow CORS

--- app.js
+++ app.js
@@ -7,7 +7,19 @@ var express = require('express'),
     path = require('path'),
     http = require('http');
 
-//this allows us to actually READ the contents of a POST request (i.e., the JSON)
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
 app.use(allowCrossDomain);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
